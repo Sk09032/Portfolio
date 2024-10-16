713e7f81dfb2b9fc6f094bacb088db09b26a226e
Sk09032 <kgpian121@iitkgp.ac.in>
2024-10-16 11:16:36 +0530

    done

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
   //typing animation script
 
   var typed = new Typed(".typing", {
-    strings: ["IITan", "Developer", "Designer"],
+    strings: ["IITan", "Data Scientist", "Data Analyst"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
